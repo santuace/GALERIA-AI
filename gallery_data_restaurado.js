1d88b614de7708fb43b82552027515e2e13e13d1
santuace <mut.santiago@gmail.com>
2025-06-24 14:23:13 -0300

    m

--- gallery_data_restaurado.js
+++ gallery_data_restaurado.js
@@ -4,84 +4,98 @@ const galleryImages = [
         type: "video",
         title: "Animación IA",
         prompt: "Video generado por IA."
+	model: "Runway"
     }, 
   {
         filename: "video2.mp4",
         type: "video",
         title: "Animación IA",
         prompt: "Video generado por IA."
+	model: "Runway"
     }, 
   {
         filename: "video4.mp4",
         type: "video",
         title: "Animación IA",
         prompt: "Video generado por IA."
+	model: "Runway"
     }, 
   {
         filename: "video5.mp4",
         type: "video",
         title: "Animación IA",
         prompt: "Video generado por IA."
+	model: "Runway"
     }, 
   {
         filename: "video6.mp4",
         type: "video",
         title: "Animación IA",
         prompt: "Video generado por IA."
+	model: "Runway"
     }, 
   {
         filename: "video7.mp4",
         type: "video",
         title: "Animación IA",
         prompt: "Video generado por IA."
+	model: "Runway"
     }, 
   {
         filename: "video8.mp4",
         type: "video",
         title: "Animación IA",
         prompt: "Video generado por IA."
+	model: "Runway"
     }, 
   {
         filename: "video9.mp4",
         type: "video",
         title: "Animación IA",
         prompt: "Video generado por IA."
+	model: "Runway"
     }, 
   {
         filename: "video10.mp4",
         type: "video",
         title: "Animación IA",
         prompt: "Video generado por IA."
+	model: "Runway"
     }, 
   {
         filename: "video11.mp4",
         type: "video",
         title: "Animación IA",
         prompt: "Video generado por IA."
+	model: "Runway"
     }, 
  {
         filename: "video12.mp4",
         type: "video",
         title: "Animación IA",
         prompt: "Video generado por IA."
+	model: "Runway"
     }, 
  {
         filename: "video13.mp4",
         type: "video",
         title: "Animación IA",
         prompt: "Video generado por IA."
+	model: "Runway"
     }, 
  {
         filename: "video14.mp4",
         type: "video",
         title: "Animación IA",
         prompt: "Video generado por IA."
+	model: "Runway"
     }, 
  {
         filename: "video15.mp4",
         type: "video",
         title: "Animación IA",
         prompt: "Video generado por IA."
+	model: "Runway"
     }, 
 ];
 
@@ -91,5 +105,6 @@ for (let i = 168; i >= 1; i--) {
         type: "image",
         title: `Creación AI #${i}`,
         prompt: `Prompt: Detalles de la creación AI número ${i}.`
+	model: "Midjourney"
     });
 }
